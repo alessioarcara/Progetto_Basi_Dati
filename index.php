1235c5ac0aa81f6e9beb068a82e2e7cf25c610bf
alessioarcara <Spammer123@>
2021-02-13 11:45:06 +0100

    file condiviso index.html

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$host='localhost';
+$db = 'Unibo';
+$username = 'postgres';
+$password = 'Toor';
+
+$dsn = "pgsql:host=$host; port=5432; dbname=$db; user=$username; password=$password";
+
+try{
+    // create a PostgreSQL database connection
+    $conn = new PDO($dsn);
+
+    // display a message if connected to the PostgreSQL successfully
+    if($conn){
+        echo "Connessione se <strong>$db</strong> riuscita!";
+    }
+}catch (PDOException $e){
+    // report error message
+    echo $e->getMessage();
+}
+?>
+
+</body>
+</html>

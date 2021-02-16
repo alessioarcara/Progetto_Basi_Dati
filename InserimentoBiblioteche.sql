12b97479922b801405bff0b652ab1b5876b9df93
Borgo99 <andrea123borgo@gmail.com>
2021-02-16 18:44:15 +0100

    Primi passi

---
+++ InserimentoBiblioteche.sql
@@ -0,0 +1,15 @@
+/* INSERT INTO BIBLIOTECA */
+
+INSERT INTO BIBLIOTECA VALUES (
+	'Biblioteca Universitaria di Bologna',
+	'Via Zamboni, 33-35 Bologna 40126 BO',
+	'bub.info@unibo.it',
+	'http://www.bub.unibo.it',
+	10,
+	20
+);
+
+
+
+
+

945a2aacd0c547bf37b82fabceda4f551092f430
Michael Carchesio <michael.carchesio@gmail.com>
2021-02-19 10:35:05 +0100

    Aggiunte prime operazioni e gestione con function

---
+++ OperazioniAmministratori/caricamentoDatiUtili.sql
@@ -0,0 +1,15 @@
+INSERT INTO UTENTE VALUES (
+    'mario.gialli@unibo.it',
+    'passwd',
+    'Mario',
+    'Gialli',
+    '1998/07/21',
+    'Pescara',
+    '+39 1211223212'
+);
+
+INSERT INTO AMMINISTRATORE VALUES (
+    'mario.gialli@unibo.it',
+    'Direttore',
+    'Biblioteca Universitaria di Bologna'
+);

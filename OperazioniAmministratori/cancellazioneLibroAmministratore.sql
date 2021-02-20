5bc92ed206b0b9c1e36d3394d02008625f20400b
Michael Carchesio <michael.carchesio@gmail.com>
2021-02-20 16:24:08 +0100

    Aggiunta operazioni ad utente Amministratore, controllare su Trello le eventuali modifiche effettuate

---
+++ OperazioniAmministratori/cancellazioneLibroAmministratore.sql
@@ -0,0 +1,33 @@
+DROP FUNCTION IF EXISTS cancellazioneLibroAmministratore;
+
+CREATE FUNCTION cancellazioneLibroAmministratore(EmailA TEXT, CodiceL TEXT, NomeBiblio TEXT)
+RETURNS BOOLEAN
+    AS $$
+    DECLARE flagBiblioteca INT;
+
+    BEGIN
+        IF EXISTS (
+            SELECT *
+            FROM AMMINISTRATORE
+            WHERE (EmailAmministratore = EmailA 
+                   AND NomeBiblioteca = NomeBiblio 
+                   AND NomeBiblioteca IN (SELECT NomeBiblioteca
+                                          FROM LIBRO
+                                          WHERE (CodiceLibro = CodiceL)))
+        ) THEN
+            DELETE FROM LIBRO WHERE (codiceLibro = CodiceL);
+            RETURN TRUE;
+        END IF;
+
+        RETURN FALSE;
+    END; $$
+LANGUAGE 'plpgsql';
+
+SELECT cancellazioneLibroAmministratore ('mario.verdi@unibo.it', '0000', 'Biblioteca di Medicina');
+
+SELECT * FROM BIBLIOTECA;
+SELECT * FROM AMMINISTRATORE;
+SELECT * FROM UTENTE;
+SELECT * FROM LIBRO;
+
+--INSERT INTO LIBRO VALUES ('0000', 'Libro1', 'NuovaEdizione', '1999', 'Giallo', 'Biblioteca di Medicina');

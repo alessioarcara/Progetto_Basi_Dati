513efc5260731cbfc21667edef929c47e5f737ae
Michael Carchesio <michael.carchesio@gmail.com>
2021-02-21 15:38:55 +0100

    Up OperazioniAmminitratori

---
+++ OperazioniAmministratori/getPrenotazioni.sql
@@ -0,0 +1,30 @@
+DROP FUNCTION IF EXISTS getPrenotazioni;
+
+CREATE FUNCTION getPrenotazioni (EmailA TEXT, NomeBiblio TEXT)
+    RETURNS TABLE (
+        Codice CHAR(10),
+        DataI DATE,
+        DataF DATE,
+        EmailU VARCHAR(50),
+        CodiceLibroC CHAR(10)
+    )
+    AS $$
+    BEGIN
+        IF EXISTS (
+            SELECT *
+            FROM AMMINISTRATORE
+            WHERE (EmailAmministratore = EmailA AND NomeBiblioteca = NomeBiblio)
+        ) THEN
+            RETURN QUERY
+            SELECT *
+            FROM PRENOTAZIONE
+            WHERE (CodiceLibroCartaceo IN (SELECT CodiceLibro
+                                           FROM LIBRO
+                                           WHERE (NomeBiblioteca = NomeBiblio)))
+        END IF;
+        RETURN NULL;
+    END; $$
+LANGUAGE 'plpgsql';
+
+SELECT * FROM LIBROCARTACEO;
+SELECT * FROM PRENOTAZIONE;

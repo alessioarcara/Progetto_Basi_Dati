ae1c6ef1533f6f24b0f246fbea1c71d82e8b5421
alessioarcara <Spammer123@>
2021-03-13 16:13:40 +0100

    DONE: buttons/js/css and ricerca first steps

---
+++ WIS/js/scriptHeader.js
@@ -0,0 +1,4 @@
+$('.chk-btn').on('click', function(){
+    $('.chk-btn').removeClass('selected');
+    $(this).addClass('selected');
+});
--- WIS/views/layout/main.php
+++ WIS/views/layout/main.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="css/Style.css">
+    <link rel="stylesheet" href="css/Style2.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -12,8 +12,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
+
     <div class="statusBar">
         <button class="btn btn-dark" type="button">Mappa Biblioteche</button>
         <div class="dropdown">
@@ -32,34 +31,28 @@
     <div class="flex-container logo colorContainerHeader">
         <a href="index.html"><img  id="logo" src="Logo.png" alt="Logo"></a>
     </div>
-
-    <div class="ricerca">
-
-        <div class="flex-container">
-            <input type="text" class="form-control" placeholder="Tutte le informazioni">
-            
-            <input type="checkbox" class="btn-check" id="btn-check-1-outlined" checked autocomplete="off">
-            <label class="btn btn-outline-secondary dimLeft2" for="btn-check-1-outlined">Biblioteche</label>
-            
-            <a href="Page2.html"><button type="button" class="btn btn-secondary dimLeft"><i class="fa fa-search"></i></button></a>
+    <div class="flex-container colorContainerHeader ricerca">
+        <form action="/action_page.php" method="post">
+            <input type="text" placeholder="Tutte le informazioni...">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+    </div>
+    <div class="flex-container colorContainerSearch search-buttons">
+        <div class="search-buttons-row_1">
+            <input type="button" id='chk-biblioteche' class='chk-btn' value="Biblioteche">
+            <input type="button" id='chk-postilettura' class='chk-btn' value="Posti Lettura">
         </div>
-        <br>
-        <div class="flex-container colorContainerSearch">
-            <input type="checkbox" class="btn-check" id="btn-check-2-outlined" autocomplete="off">
-            <label class="btn btn-outline-dark" for="btn-check-2-outlined"><i class="fa fa-user-o"></i> Posto lettura</label>
-
-            <input type="checkbox" class="btn-check" id="btn-check-3-outlined" autocomplete="off">
-            <label class="btn btn-outline-primary dimLeft" for="btn-check-3-outlined"><i class="fa fa-archive"></i> Biblioteca</label>
-
-            <input type="checkbox" class="btn-check" id="btn-check-4-outlined" autocomplete="off">
-            <label class="btn btn-outline-danger dimLeft" for="btn-check-4-outlined"><i class="fa fa-book"></i> Libro cartaceo</label>
-
-            <input type="checkbox" class="btn-check" id="btn-check-5-outlined" autocomplete="off">
-            <label class="btn btn-outline-success dimLeft" for="btn-check-5-outlined"><i class="fa fa-file-pdf-o"></i> Ebook</label>
+        <div class="search-buttons-row_2">
+            <input type="button" id='chk-libricartacei' class='chk-btn' value="Libri Cartacei">
+            <input type="button" id='chk-ebooks' class='chk-btn' value="Ebooks">
         </div>
     </div>
+</head>
+<body>
 
 {{content}}
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/scriptHeader.js"></script>
 </body>
 </html>

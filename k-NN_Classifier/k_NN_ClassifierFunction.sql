8059dede37cde3d88ef870830bec26998b99a2cf
alessioarcara <Spammer123@>
2021-03-18 23:54:08 +0100

    inizializzazione k_NN means _ executeQuery, Dictionary, Dataframe

--- k-NN_Classifier/k_NN_ClassifierFunction.sql
+++ k-NN_Classifier/k_NN_ClassifierFunction.sql
@@ -1,10 +1,43 @@
+/*
+segmentare
+età e professione utenti utilizzatori
+numero e genere richieste prestiti libri cartacei
+*/
 DROP FUNCTION pymax()
 
 CREATE OR REPLACE FUNCTION pymax ()
-    RETURNS TABLE (like utilizzatore)
+    RETURNS JSON
 AS $$
-    rv = plpy.execute("SELECT * FROM utilizzatore")
-    return rv
+    import pandas as pd
+    import numpy as np
+    from sklearn import datasets
+
+    dict_utilizzatore = {}
+
+    etichetta_utilizzatore = plpy.execute("""SELECT utilizzatore.emailutilizzatore, professione, datadinascita, CountPrenotazioni
+                                         FROM utilizzatore
+                                         JOIN utente ON emailutente = utilizzatore.emailutilizzatore
+                                         JOIN (SELECT count(*) AS CountPrenotazioni, prenotazione.emailutilizzatore
+                                               FROM prenotazione
+                                               GROUP BY prenotazione.emailutilizzatore) AS A
+                                               ON utilizzatore.emailutilizzatore = A.emailutilizzatore
+                                         """)
+
+    for r in etichetta_utilizzatore:
+        dict_utilizzatore[r["emailutilizzatore"]] = [r["professione"], r["datadinascita"], r["countprenotazioni"]]
+
+    df = pd.DataFrame(dict_utilizzatore,
+                      columns=['professione', 'datadinascita', 'CountPrenotazioni'],
+                      index=dict_utilizzatore.keys())
+
+
+
+
+    return df.to_json()
 $$ LANGUAGE plpython3u;
 
 select pymax()
+
+SELECT count(*) AS CountPrenotazioni, emailutilizzatore
+FROM prenotazione
+GROUP BY emailutilizzatore

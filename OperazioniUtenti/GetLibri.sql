6490e551d23563e20ec9ee2a27f9039fcedbbb13
alessioarcara <Spammer123@>
2021-04-11 15:06:18 +0200

    DONE: Query per ricercare libri per titolo

---
+++ OperazioniUtenti/GetLibri.sql
@@ -0,0 +1,80 @@
+DROP FUNCTION IF EXISTS LibriBiblioteca;
+CREATE FUNCTION LibriBiblioteca (VARIADIC words TEXT[] DEFAULT NULL)
+    RETURNS TABLE(
+                     codicelibrocartaceo char,
+                     titolocartaceo varchar,
+                     nomeedizionecartaceo varchar,
+                     annopubblicazionecartaceo smallint,
+                     generecartaceo varchar,
+                     nomebibliotecacartaceo varchar,
+                     statoprestito text,
+                     statoconservazione text,
+                     numpagine smallint,
+                     numscaffale smallint,
+                     codiceebook char,
+                     titoloebook varchar,
+                     nomeedizioneebook varchar,
+                     annopubblicazioneebook smallint,
+                     genereebook varchar,
+                     nomebibliotecaebook varchar,
+                     dimensione float,
+                     pdf varchar,
+                     numaccessischeda int
+                 )
+AS $$
+BEGIN
+    IF words ISNULL THEN
+        RETURN QUERY
+            SELECT t1.codicelibro AS codicelibrocartaceo,
+                   t1.titolo AS titolocartaceo,
+                   t1.nomeedizione AS nomeedizionecartaceo,
+                   t1.annopubblicazione AS annopubblicazionecartaceo,
+                   t1.genere AS generecartaceo,
+                   t1.nomebiblioteca AS nomebibliotecacartaceo,
+                   t1.statoprestito::text AS statoprestito,
+                   t1.statoconservazione::text AS statoconservazione,
+                   t1.numpagine AS numpagine,
+                   t1.numscaffale AS numscaffale,
+                   t2.codicelibro AS codiceebook,
+                   t2.titolo AS titoloebook,
+                   t2.nomeedizione AS nomeedizioneebook,
+                   t2.annopubblicazione AS annopubblicazioneebook,
+                   t2.genere AS genereebook,
+                   t2.nomebiblioteca AS nomebibliotecaebook,
+                   t2.dimensione AS dimensione,
+                   t2.pdf AS pdf,
+                   t2.numaccessischeda AS numaccessischeda
+            FROM (SELECT * FROM libro l JOIN librocartaceo on l.codicelibro = librocartaceo.codicelibrocartaceo) T1
+                 FULL JOIN (SELECT * FROM libro l JOIN ebook e on l.codicelibro = e.codiceebook) T2
+                           ON T1.codicelibro = T2.codicelibro;
+    ELSEIF EXISTS(SELECT * FROM libro l WHERE titolo ~~*  ANY (SELECT concat('%',w,'%') FROM unnest(array[words]) w)) THEN
+        RETURN QUERY
+        SELECT t1.codicelibro AS codicelibrocartaceo,
+               t1.titolo AS titolocartaceo,
+               t1.nomeedizione AS nomeedizionecartaceo,
+               t1.annopubblicazione AS annopubblicazionecartaceo,
+               t1.genere AS generecartaceo,
+               t1.nomebiblioteca AS nomebibliotecacartaceo,
+               t1.statoprestito::text AS statoprestito,
+               t1.statoconservazione::text AS statoconservazione,
+               t1.numpagine AS numpagine,
+               t1.numscaffale AS numscaffale,
+               t2.codicelibro AS codiceebook,
+               t2.titolo AS titoloebook,
+               t2.nomeedizione AS nomeedizioneebook,
+               t2.annopubblicazione AS annopubblicazioneebook,
+               t2.genere AS genereebook,
+               t2.nomebiblioteca AS nomebibliotecaebook,
+               t2.dimensione AS dimensione,
+               t2.pdf AS pdf,
+               t2.numaccessischeda AS numaccessischeda
+        FROM (SELECT * FROM libro l JOIN librocartaceo on l.codicelibro = librocartaceo.codicelibrocartaceo) T1
+                 FULL JOIN (SELECT * FROM libro l JOIN ebook e on l.codicelibro = e.codiceebook) T2
+                           ON T1.codicelibro = T2.codicelibro
+        WHERE t1.titolo ~~*  ANY (SELECT concat('%',w,'%') FROM unnest(array[words]) w);
+    END IF;
+END; $$
+LANGUAGE 'plpgsql';
+
+SELECT * FROM LibriBiblioteca();
+SELECT * FROM LibriBiblioteca('dsadsadasd')

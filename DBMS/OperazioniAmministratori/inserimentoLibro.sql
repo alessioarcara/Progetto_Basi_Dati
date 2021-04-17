af6fed8beb190fd387eb2919e83b66fe89bce7d3
Michael Carchesio <michael.carchesio@gmail.com>
2021-04-17 15:55:47 +0200

    Inizio settaggio inserimento libro cartaceo con controllo ad ebook, amministratore e libro

--- DBMS/OperazioniAmministratori/inserimentoLibro.sql
+++ DBMS/OperazioniAmministratori/inserimentoLibro.sql
@@ -1,27 +1,70 @@
-DROP FUNCTION IF EXISTS inserimentoLibroAmministratore;
+--DROP FUNCTION IF EXISTS inserimentoLibroAmministratore;
 
-CREATE FUNCTION inserimentoLibroAmministratore (EmailA TEXT, CodiceL TEXT, TitoloL TEXT, Edizione TEXT, Pubblicazione SMALLINT, GenereL TEXT, NomeBiblio TEXT)
-RETURNS BOOLEAN
-    AS $$
-    DECLARE flagBiblioteca INT;
+--CREATE FUNCTION inserimentoLibroAmministratore (EmailA TEXT, CodiceL TEXT, TitoloL TEXT, Edizione TEXT, Pubblicazione SMALLINT, GenereL TEXT, NomeBiblio TEXT)
+--RETURNS BOOLEAN
+--    AS $$
+--    DECLARE flagBiblioteca INT;
+--
+--    BEGIN
+--        IF EXISTS (
+--            SELECT *
+--            FROM AMMINISTRATORE
+--            WHERE (EmailAmministratore = EmailA AND NomeBiblioteca = NomeBiblio)
+--        ) THEN
+--            INSERT INTO LIBRO VALUES (CodiceL, TitoloL, Edizione, Pubblicazione, GenereL, NomeBiblio);
+--            RETURN TRUE;
+--        END IF;
+--
+--        RETURN FALSE;
+--    END; $$
+--LANGUAGE 'plpgsql';
 
-    BEGIN
-        IF EXISTS (
-            SELECT *
-            FROM AMMINISTRATORE
-            WHERE (EmailAmministratore = EmailA AND NomeBiblioteca = NomeBiblio)
-        ) THEN
-            INSERT INTO LIBRO VALUES (CodiceL, TitoloL, Edizione, Pubblicazione, GenereL, NomeBiblio);
-            RETURN TRUE;
-        END IF;
 
-        RETURN FALSE;
-    END; $$
-LANGUAGE 'plpgsql';
 
-SELECT inserimentoLibroAmministratore ('mario.verdi@unibo.it', 'A123B', 'Titolo-1', 'Giunti', '1997', 'Azione', 'Biblioteca di Medicina');
+DROP FUNCTION IF EXISTS inserimentoLibroCartaceo;
+CREATE FUNCTION inserimentoLibroCartaceo (EmailA TEXT, CodLibro TEXT, TitoloL TEXT, NomeEd TEXT, Pubblicazione SMALLINT, GenereL TEXT, StatoPrest PRESTITO, StatoCons CONSERVAZIONE, NPagine SMALLINT, NScaffale SMALLINT, NomeBiblio TEXT)
+    RETURNS BOOLEAN
+	AS $$
+	DECLARE flagAmministratore INT;
+	DECLARE flagEbook INT;
+	DECLARE flagLibro INT;
+	
+	BEGIN
+		--Controllo che il libro cartaceo non sia stato inserito
+		SELECT COUNT(*) INTO flagLibro
+		FROM LIBROCARTACEO
+		WHERE (codicelibrocartaceo = CodLibro);
+		
+		--Controllo che l'amministratore sia presente
+		SELECT COUNT(*) INTO flagAmministratore
+		FROM AMMINISTRATORE
+		WHERE (emailamministratore = EmailA);
+		
+		--Controllo che l'ebook non sia presente sfruttando un flag
+		SELECT COUNT(*) INTO flagEbook
+		FROM EBOOK
+		WHERE (codiceebook = CodLibro);
+		
+		IF (flagLibro = 0 AND flagAmministratore = 1 AND flagEbook = 0) THEN
+			INSERT INTO LIBRO VALUES (CodLibro, TitoloL, NomeEd, Pubblicazione, GenereL, NomeBiblio);
+			INSERT INTO LIBROCARTACEO VALUES (CodLibro, StatoPrest, StatoCons, NPagine, NScaffale);
+			RETURN TRUE;
+		END IF;
+		
+		RETURN FALSE;
+	END; $$
+LANGUAGE 'plpgsql';			
 
-SELECT * FROM BIBLIOTECA;
-SELECT * FROM AMMINISTRATORE;
-SELECT * FROM UTENTE;
+SELECT inserimentoLibroCartaceo ('elena.bianchi@unibo.it', '90J0GHJKL3', 'Il fu Mattia Pascal', 'Giunti', '2010', 'Romanzo', 'DISPONIBILE', 'OTTIMO', '124', '12', 'Biblioteca Universitaria di Bologna');
+
+
 SELECT * FROM LIBRO;
+SELECT * FROM LIBROCARTACEO;
+SELECT * FROM EBOOK;
+		
+		
+		
+		
+		
+		
+	

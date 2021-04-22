84d2eb321577fbb098a0c90c8205ab3e26dc05b0
alessioarcara <Spammer123@>
2021-04-22 12:41:58 +0200

    DONE: MongoDB

--- WIS/Log/Log.php
+++ WIS/Log/Log.php
@@ -2,7 +2,7 @@
     class Log {
         
         public function writeLog ($user, $string) {
-            $conn = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+            $conn = new MongoDB\Driver\Manager("mongodb://argonauta.synology.me:32774");
             $bulk = new MongoDB\Driver\BulkWrite;
             $bulk -> insert([
                 'user' => $user,
